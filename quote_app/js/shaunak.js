cdc7e15b517d1371f3602b9890263d7818006c3e
Shaunak Sen <shaunak1105@gmail.com>
2016-07-08 19:18:07 +0530

    completed theme change functionality

--- quote_app/js/shaunak.js
+++ quote_app/js/shaunak.js
@@ -5,6 +5,7 @@ function getQuote() {
     $("#quote-text").fadeOut(300);
     $("#quote-author").fadeOut(300);
     $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?", function (data) {
+        changeTheme();
         console.log(data);
         quote = data.quoteText;
         author = data.quoteAuthor;
@@ -19,19 +20,42 @@ function getQuote() {
 function getQuoteByCategory(category) {
     if (category == "funny" || category == "love" || category == "sports" || category == "inspire") {
         var url = "http://quotes.rest/qod.json?category=" + category;
+        $("#quote-text").fadeOut(300);
+        $("#quote-author").fadeOut(300);
         $.getJSON(url, function (data) {
-            var quoteObject = data.contents.quotes[0]
+            changeTheme();
+            var quoteObject = data.contents.quotes[0];
             console.log(quoteObject);
+            var quote = quoteObject.quote;
+            var author = quoteObject.author;
+            if (author == "") {
+                author = "Annonymous"
+            }
+            $("#quote-text").html(quote).fadeIn(300);
+            $("#quote-author").html(author).fadeIn(300);
         })
     }
+}
+// ce93d8 7e57c2
+colors = ['#c77f7f', '#ce93d8', '#74a3c9', '#7e57c2', '#aed581', '#ce8973', '#c8c121', '#81c784', '#ba5dce'];
+
+function changeTheme() {
+    var random = Math.floor(Math.random() * 10);
+    random = random == 9 ? 8 : random;
+    var themeColor = colors[random];
+    var body = document.body;
+    body.style.transition = "background 0.3s linear";
+    body.style.backgroundColor = themeColor;
 }
 
 $(document).ready(function () {
     getQuote();
     $("#new-quote").click(function () {
         getQuote();
+
     });
     $(".custom-button").click(function () {
+
         var category = this.id;
         getQuoteByCategory(category);
     })
