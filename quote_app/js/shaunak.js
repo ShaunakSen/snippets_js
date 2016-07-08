8cedefa98de7daf37507deffa977dbd87bf8ad86
Shaunak Sen <shaunak1105@gmail.com>
2016-07-08 16:19:17 +0530

    getting random quote from API done

--- quote_app/js/shaunak.js
+++ quote_app/js/shaunak.js
@@ -1,5 +1,24 @@
-$(document).ready(function () {
-    $.getJSON("http://quotes.rest/qod.json", function (data) {
+function getQuote() {
+    console.log("here");
+    var quote = "";
+    var author = "";
+    $("#quote-text").fadeOut(300);
+    $("#quote-author").fadeOut(300);
+    $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?", function (data) {
         console.log(data);
+        quote = data.quoteText;
+        author = data.quoteAuthor;
+        if(author == ""){
+            author = "Annonymous"
+        }
+        $("#quote-text").html(quote).fadeIn(300);
+        $("#quote-author").html(author).fadeIn(300);
     })
+}
+$(document).ready(function () {
+    getQuote();
+    $("#new-quote").click(function () {
+        getQuote();
+    });
+
 });
