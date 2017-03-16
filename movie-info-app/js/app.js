fc4cd89641790a2ab85b5e5bf0397a14b9ab2e9f
Shaunak Sen <shaunak1105@gmail.com>
2017-03-17 01:02:02 +0530

    getting data using axios module

--- movie-info-app/js/app.js
+++ movie-info-app/js/app.js
@@ -1,3 +1,21 @@
-/**
- * Created by DELL on 17-03-2017.
- */
+$(document).ready( () => {
+    // listen for submission of form
+
+    $('#searchForm').on('submit', (e) => {
+        let searchText = $('#searchText').val();
+        getMovies(searchText);
+        e.preventDefault();
+    });
+
+    function getMovies(searchText) {
+        // console.log(searchText);
+
+        // REQUEST API
+
+        axios.get('http://www.omdbapi.com?s=' + searchText).then((response)=>{
+            console.log(response);
+        }).catch((err)=>{
+            console.log(err);
+        });
+    }
+});
