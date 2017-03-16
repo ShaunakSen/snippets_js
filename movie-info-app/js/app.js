d7ff4c7229ad6c7581659d2bdfc08045e5091368
Shaunak Sen <shaunak1105@gmail.com>
2017-03-17 02:11:19 +0530

    finished the app..

--- movie-info-app/js/app.js
+++ movie-info-app/js/app.js
@@ -7,34 +7,103 @@ $(document).ready( () => {
         e.preventDefault();
     });
 
-    function getMovies(searchText) {
-        // console.log(searchText);
+});
 
-        // REQUEST API
+function getMovies(searchText) {
+    // console.log(searchText);
 
-        axios.get('http://www.omdbapi.com?s=' + searchText).then((response)=>{
-            console.log(response);
-            let movieData = response.data.Search;
-            let output = '';
+    // REQUEST API
 
-            $.each(movieData, (index, movie)=>{
-                output += `
+    axios.get('http://www.omdbapi.com?s=' + searchText).then((response)=>{
+        console.log(response);
+        let movieData = response.data.Search;
+        let output = '';
+
+        $.each(movieData, (index, movie)=>{
+            output += `
                     <div class="col-md-3">
                         <div class="well text-center">
                         <img src="${movie.Poster}" alt="">
                             <h5>${movie.Title}</h5>
-                            <a onclick="movieSelection('${movie.imdbID}')" class="btn btn-primary" href="#">
+                            <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">
                                 Movie Details
                             </a>
                         </div>
                      </div>
                 `
-            });
-
-            $('#movies').html(output);
-
-        }).catch((err)=>{
-            console.log(err);
         });
-    }
-});
+
+        $('#movies').html(output);
+
+    }).catch((err)=>{
+        console.log(err);
+    });
+}
+
+function movieSelected(id){
+    sessionStorage.setItem('movieId', id);
+    window.location = 'movie.html';
+    return false;
+}
+
+function getMovie() {
+    let movieId = sessionStorage.getItem('movieId');
+
+    axios.get('http://www.omdbapi.com?i=' + movieId).then((response)=>{
+        console.log(response);
+
+        let movie = response.data;
+
+        // Create output
+        let output = `
+            <div class="row">
+                <div class="col-md-4">
+                    <img src="${movie.Poster}" class="thumbnail" alt="">
+                </div>
+                <div class="col-md-8">
+                    <h2>${movie.Title}</h2>
+                    <ul class="list-group">
+                        <li class="list-group-item">
+                            <strong>Genre:</strong> ${movie.Genre}
+                        </li>
+                        <li class="list-group-item">
+                            <strong>Released:</strong> ${movie.Released}
+                        </li>
+                        <li class="list-group-item">
+                            <strong>Rated:</strong> ${movie.Rated}
+                        </li>
+                        <li class="list-group-item">
+                            <strong>IMDB Rating:</strong> ${movie.imdbRating}
+                        </li>
+                        <li class="list-group-item">
+                            <strong>Director:</strong> ${movie.Director}
+                        </li>
+                        <li class="list-group-item">
+                            <strong>Writer:</strong> ${movie.Writer}
+                        </li>
+                        <li class="list-group-item">
+                            <strong>Actors:</strong> ${movie.Actors}
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row">
+                <div class="well">
+                    <h3>Plot</h3>
+                    ${movie.Plot}
+                    <hr>
+                    <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">
+                    View in IMDB
+                    </a>
+                    <a href="index.html" class="btn btn-default">Go Back</a>
+                </div>
+            </div>
+        `;
+
+        $('#movie').html(output);
+
+
+    }).catch((err)=>{
+        console.log(err);
+    });
+}
