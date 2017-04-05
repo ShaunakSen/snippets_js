a61f94c2c50f128fbe06bbed387bc0265dbf80c9
Shaunak Sen <shaunak1105@gmail.com>
2017-04-05 21:11:05 +0530

    API params tested

--- youtube-app/js/app.js
+++ youtube-app/js/app.js
@@ -1,3 +1,30 @@
-/**
- * Created by DELL on 05-04-2017.
- */
+var apiKey = 'AIzaSyCcx6V_hKT9RRHINxUib0sxNdGax3_s9-k';
+var url = 'https://www.googleapis.com/youtube/v3/search';
+
+function search() {
+
+    // Clear the DOM
+    $('#results').html('');
+    $('#buttons').html('');
+
+    // Get form input
+
+    var query = $('#query').val();
+
+    // GET REQUEST
+
+    $.get(
+        url, {
+            part: 'snippet, id',
+            q: 'rockabye',
+            type: 'video',
+            key: apiKey
+        },
+        function (data) {
+            console.log(data);
+        }
+    )
+
+}
+
+search();
