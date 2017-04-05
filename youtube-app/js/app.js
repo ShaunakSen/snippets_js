f53f11d140edd6e3aa7747bd68090db45f311fd0
ShaunakSen <shaunak1105@gmail.com>
2017-04-06 02:00:48 +0530

    building the output

--- youtube-app/js/app.js
+++ youtube-app/js/app.js
@@ -1,6 +1,16 @@
 var apiKey = 'AIzaSyCcx6V_hKT9RRHINxUib0sxNdGax3_s9-k';
 var url = 'https://www.googleapis.com/youtube/v3/search';
 
+$(document).ready(function () {
+    $('#search-form').submit(function (e) {
+        e.preventDefault();
+    });
+
+    $('#submit-button').click(function () {
+        search();
+    })
+});
+
 function search() {
 
     // Clear the DOM
@@ -16,15 +26,33 @@ function search() {
     $.get(
         url, {
             part: 'snippet, id',
-            q: 'rockabye',
+            q: query,
             type: 'video',
             key: apiKey
         },
         function (data) {
             console.log(data);
+            var nextPageToken = data.nextPageToken;
+            var prevPageToken = data.prevPageToken;
+
+            $.each(data.items, function (index, item) {
+                var output = buildOutput(item);
+                // Display Results
+                $('#results').append(output);
+            });
         }
     )
+}
+
+function buildOutput(item) {
+
+    var videoId = item.id.videoId;
+    var title = item.snippet.title;
+    var description = item.snippet.description;
+    var thumbnail = item.snippet.thumbnails.high.url;
+    var channelTitle = item.snippet.channelTitle;
+    var videoDate = item.snippet.publishedAt;
+
 
 }
 
-search();
